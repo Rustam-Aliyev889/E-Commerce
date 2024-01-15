e95a2218ce9900dfd378b450bba34798f19c4d4d
Rustam Aliyev <rustam.aliyev889@gmail.com>
2024-01-15 14:11:58 +0000

    registration templates and password update func

--- radiant_rituals/settings.py
+++ radiant_rituals/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'products',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -28,7 +29,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'products',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +120,15 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+# SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587  
+EMAIL_HOST_USER = 'jj12blaster@gmail.com'
+EMAIL_HOST_PASSWORD = 'hyfymderkicaavik'
